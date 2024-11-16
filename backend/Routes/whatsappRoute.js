693a72db77b621ee9d10b705edbae9bd7a77ef81
asherDn-Official <developers@asherdn.com>
2024-11-16 14:56:13 +0530

    Delete backend directory

--- backend/Routes/whatsappRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const sendwhatsappController = require("../Controllers/whatsappController");
-
-// Define route to send the WhatsApp message
-router.post("/send-whatsapp", sendwhatsappController.sendWhatsApp);
-
-module.exports = router;
